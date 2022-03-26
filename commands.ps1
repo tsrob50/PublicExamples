2cb8d1114e43ddaec466d00abbeacec2b8c9ee2b
Travis Roberts <27257343+tsrob50@users.noreply.github.com>
2022-03-26 18:59:31 -0500

    Create commands.ps1

---
+++ commands.ps1
@@ -0,0 +1,53 @@
+# Deployment variables 
+$location = 'CentralUS'
+$projectRg = 'WebAppProjectRG'
+
+# Create the deployment Resouce Group
+New-AzResourceGroup `
+    -Location $location `
+    -Name $projectRg
+
+New-AzResourceGroupDeployment `
+    -ResourceGroupName $projectRg `
+    -Templatefile azuredeploy.json `
+    -projectName 'cirTest' `
+    -storageSKU  'Standard_ZRS'
+
+# Create the Template Spec
+# Template Spec variables
+$tempSpecsName = 'WebAppTempSpecs'
+$tempSpecsRg = 'templateSpecsRG'
+$tempSpecsVer = '1.3'
+
+# Create the resouce group
+New-AzResourceGroup `
+    -Location $location `
+    -Name $tempSpecsRg
+
+# Create the Template Spec
+New-AzTemplateSpec `
+    -Name $tempSpecsName `
+    -Version $tempSpecsVer `
+    -ResourceGroupName $tempSpecsRg `
+    -Location $location `
+    -TemplateFile .\azuredeploy.json
+
+# Deploy with Teplate Spec
+# Get the Template Specs ID
+$templateSpecID = (Get-AzTemplateSpec -ResourceGroupName $tempSpecsRg -Name $tempSpecsName -Version $tempSpecsVer).Versions.Id
+
+# Run the deployment
+New-AzResourceGroupDeployment `
+    -ResourceGroupName $projectRg `
+    -TemplateSpecId $templateSpecID `
+    -projectName 'cirTest' `
+    -storageSKU  'Standard_ZRS'
+
+
+# Run the deployment with tags
+New-AzResourceGroupDeployment `
+    -ResourceGroupName $projectRg `
+    -TemplateSpecId $templateSpecID `
+    -projectName 'cirTest' `
+    -storageSKU  'Standard_ZRS' `
+    -tags @{Environment = "Lab"}
